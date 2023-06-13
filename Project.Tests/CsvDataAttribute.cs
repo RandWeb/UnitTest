3a77b1119c37bd0d7deb3d916a2e7edc9ed6a2ea
مهرداد تابش <mehrdadit12@gmail.com>
2023-06-13 07:51:32 -0700

    usege custom Attribute

---
+++ Project.Tests/CsvDataAttribute.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection;
+using Xunit.Sdk;
+
+namespace InlineDataAttribute.Tests;
+
+[AttributeUsage(AttributeTargets.Method,AllowMultiple = false,Inherited = false)]
+public class CsvDataAttribute:DataAttribute
+{
+    private string _filePath;
+    private char _separator;
+    private List<object[]> _csvData= new();
+
+    public CsvDataAttribute(string filePath, char separator)
+    {
+        _filePath = filePath;
+        _separator = separator;
+    }
+
+    private void readFile(MethodInfo methodInfo)
+    {
+        Type[] parameterTypes =
+            methodInfo.GetParameters().OrderBy(p => p.Position).Select(t => t.ParameterType).ToArray();
+        using (var reader = new StreamReader(_filePath))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (line!=null)
+                {
+                    var values = line.Split(_separator).Cast<object>().ToArray();
+                    var convertedValues = new List<object>();
+
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        var type = parameterTypes[i];
+                        var val = values[i];
+                        if (type.IsEnum)
+                        {
+                            var value = Convert.ToInt32(val);
+                            var convertableValue = Enum.ToObject(type, value);
+                            var enumObj = Convert.ChangeType(convertableValue, type);
+                            
+                            convertedValues.Add(enumObj);
+                        }
+                        else
+                        {
+                         convertedValues.Add(Convert.ChangeType(val,type));   
+                        }
+                    }
+                    _csvData.Add(convertedValues.ToArray());
+                }
+            }
+        }
+    }
+    
+    public override IEnumerable<object[]> GetData(MethodInfo methodInfo)
+    {
+        if (methodInfo == null)
+        {
+            throw new ArgumentNullException();
+        }
+        readFile(methodInfo);
+        return _csvData;
+    }
+}
---
+++ Project.Tests/CustomDataAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace InlineDataAttribute.Tests;
+
+public class CustomDataAttribute
+{
+    [Theory]
+    [CsvData(@"",',')]
+    public void CreateInvoice_UsingCustomDataAttribute_ShouldNotNullAndInvoiceNotEqualZero(TypeOfService typeOfService, decimal amount)
+    {
+        InvoiceService invoiceService = new();
+
+        Invoice invoice = invoiceService.CreateInvoice(typeOfService, amount);
+        Assert.NotNull(invoice);
+        Assert.True(invoice.Id > 0);
+    }
+    [Theory]
+    [CsvData(@"",',')]
+    public void CreateInvoice_UsingJsonData_ShouldNotNullAndInvoiceNotEqualZero(TypeOfService typeOfService, decimal amount)
+    {
+        InvoiceService invoiceService = new();
+
+        Invoice invoice = invoiceService.CreateInvoice(typeOfService, amount);
+        Assert.NotNull(invoice);
+        Assert.True(invoice.Id > 0);
+    }
+}
