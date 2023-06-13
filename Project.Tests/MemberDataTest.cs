0413efd3350fa02dc2d7dd0280262856cc2a8f99
مهرداد تابش <mehrdadit12@gmail.com>
2023-06-13 07:01:26 -0700

    usege DataClass

--- Project.Tests/MemberDataTest.cs
+++ Project.Tests/MemberDataTest.cs
@@ -95,3 +95,25 @@ public class MemberDataTest
         Assert.True(invoice.Id > 0);
     }
 }
+
+public class TestDataClass
+{
+    public static IEnumerable<object[]> PropertyMemberData
+    {
+        get
+        {
+            yield return new object[]
+            {
+                TypeOfService.JobLicense,
+                1000
+            };
+            
+            yield return new object[]
+            {
+                TypeOfService.JobLicense,
+                5000
+            };
+
+        }
+    }
+}
